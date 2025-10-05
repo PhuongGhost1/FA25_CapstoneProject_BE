5c14d935d3697952af60bd7162d845c69a66b60e
PhuongGhost1 <trngphng94@gmail.com>
2025-10-05 12:19:42 +0700

    Finish writing api for update personal information

--- FA25_CusomMapOSM_BE/CusomMapOSM_Application/Interfaces/Features/User/IUserService.cs
+++ FA25_CusomMapOSM_BE/CusomMapOSM_Application/Interfaces/Features/User/IUserService.cs
@@ -1,4 +1,5 @@
 using DomainUser = CusomMapOSM_Domain.Entities.Users;
+using CusomMapOSM_Application.Models.DTOs.Features.User;
 using Optional;
 using ErrorCustom = CusomMapOSM_Application.Common.Errors;
 
@@ -8,4 +9,5 @@ public interface IUserService
 {
     Task<Option<DomainUser.User, ErrorCustom.Error>> GetUserByIdAsync(Guid userId, CancellationToken ct);
     Task<Option<DomainUser.User, ErrorCustom.Error>> GetUserByEmailAsync(string email, CancellationToken ct);
+    Task<Option<UpdateUserPersonalInfoResponse, ErrorCustom.Error>> UpdateUserPersonalInfoAsync(Guid userId, UpdateUserPersonalInfoRequest request, CancellationToken ct);
 }
